989fb810c175a8827cedb8afc151bb62e0d61e0c
iMocco <taoxin167@gmail.com>
2016-06-14 23:21:19 +0800

    添加基础代码

---
+++ my-project/src/components/Hello.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <group>
+      <cell title="记账" value="add"></cell>
+      <cell title="查账" value="sel"></cell>
+      <cell title="算账" value="ana"></cell>
+      <cell title="销账" value="del"></cell>
+    </group>
+  </div>
+</template>
+
+<script>
+import Group from 'vux/dist/components/group'
+import Cell from 'vux/dist/components/cell'
+
+export default {
+  components: {
+    Group,
+    Cell
+  }
+}
+</script>
+
+<style>
+@import '~vux/dist/vux.css';
+</style>
