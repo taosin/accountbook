3290f0e2596a5cab7a0d051dfcee86962368e6bf
iMocco <taoxin167@gmail.com>
2016-06-15 20:17:44 +0800

    添加index页面

---
+++ my-project/src/views/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <tabbar>
+      <tabbar-item>
+        <img slot="icon" >
+        <span slot="label">Wechat</span>
+      </tabbar-item>
+      <tabbar-item show-dot>
+        <img slot="icon" >
+        <span slot="label">Message</span>
+      </tabbar-item>
+      <tabbar-item selected>
+        <img slot="icon" >
+        <span slot="label">Explore</span>
+      </tabbar-item>
+      <tabbar-item>
+        <img slot="icon" >
+        <span slot="label">News</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import { Tabbar, TabbarItem } from 'vux'
+export default {
+  components: {
+    Tabbar,
+    TabbarItem
+  }
+}
+</script>
